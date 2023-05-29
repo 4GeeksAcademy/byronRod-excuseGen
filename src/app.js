803d187b9f82cfe15cab453d4106a7fb5fc8602b
Byron Rodriguez <34363257+kitsunemura@users.noreply.github.com>
2023-05-29 23:36:40 +0000

    puras excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  var who = ["The dog", "My grandma", "His turtle", "My bird"];
+  var action = ["ate", "peed", "crushed", "broke"];
+  var what = ["my homework", "the keys", "the car"];
+  var when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var randomWho = who[Math.floor(Math.random() * who.length)];
+  var randomAction = action[Math.floor(Math.random() * action.length)];
+  var randomWhat = what[Math.floor(Math.random() * what.length)];
+  var randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  var excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  document.getElementById("excuse").innerHTML = excuse;
+}
